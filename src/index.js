408e1c06fa5c8762449b4262b34de2a48b896e22
Tiago Facco <thifacco@gmail.com>
2022-10-06 09:14:00 -0300

    Append element

--- src/index.js
+++ src/index.js
@@ -11,8 +11,5 @@ import '@fortawesome/fontawesome-free/scss/regular.scss';
 import '@fortawesome/fontawesome-free/scss/brands.scss';
 import { SampleMusicComponent } from '../sampleMusic/sampleMusicComponent.js';
 
-function component() {
-  const element = document.createElement('app-sample-music');
-  return element;
-}
-document.body.appendChild(component());
+const element = document.createElement('app-sample-music');
+document.body.appendChild(element);
